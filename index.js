8ea8f515749c7c9b4383fe1497052874670b41b9
AidaCodez <211214391+AidaCodez@users.noreply.github.com>
2025-11-01 19:08:26 +0800

    feat(linkedlist): learn linked list data structure
    singly linked list can traverse foward
    doubly linked list can traverse foward and backward
    need to loop to get the index, slower than array
    but add node and remove node, faster than array

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ console.log(stack.peek());
 console.log(stack.pop());
 console.log(stack.peek());*/
 
-class Queue{
+/*class Queue{
     // items = [];
 
     // enqueue(element){
@@ -168,5 +168,198 @@ queue.enqueue('blue');
 queue.enqueue('yellow');
 console.log(queue.peek());
 console.log(queue.dequeue());
-console.log(queue.peek());
+console.log(queue.peek());*/
+
+/*//singly linked list
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+const node = new Node(111);
+console.log(node);
+
+class LinkedList{
+    constructor(){
+        this.size = 0;
+        this.head = null;
+    }
+    add(value){
+        const newNode = new Node(value);
+        if (this.size === 0){
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+            while(currentNode.next){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        this.size++;
+    }
+    //this is 0(n) complexity due to need loop to access the index 
+    getByIndex(index){
+        let position = 0;
+        let currentNode = this.head;
+
+        while(position < index){
+            currentNode = currentNode.next;
+            position++;
+        }
+        return currentNode;
+    }
+
+    addByIndex(index, value){
+        const newNode = new Node(value);
+
+        if(this.size === 0){
+            this.head = newNode;
+            return;
+        }
+
+        const previousNode = this.getByIndex(index -1)
+        newNode.next = previousNode.next;
+        previousNode.next = newNode;
+        this.size++;
+    }
+
+    removeByIndex(index){
+        let currentNode = this.head;
+        if (this.size === 0){
+            this.head = currentNode.next;
+        } else {
+            const previousNode = this.getByIndex(index -1);
+            previousNode.next = previousNode.next.next;
+
+        }
+
+    }
+}
+
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.addByIndex(1, 5);
+linkedList.removeByIndex(1);
+console.log(linkedList.getByIndex(1));
+console.log(linkedList);*/
+
+//doubly linked list
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    add(value) {
+        const newNode = new Node(value);
+        if (this.size === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
+
+    getByIndex(index) {
+        if (index < 0 || index >= this.size) return null;
+
+        let currentNode;
+        // Optimization: traverse from head or tail depending on index position
+        if (index < this.size / 2) {
+            currentNode = this.head;
+            for (let i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = this.tail;
+            for (let i = this.size - 1; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
+        }
+        return currentNode;
+    }
+
+    addByIndex(index, value) {
+        if (index < 0 || index > this.size) return;
+
+        const newNode = new Node(value);
+
+        if (index === 0) {
+            // Insert at head
+            if (this.head) {
+                newNode.next = this.head;
+                this.head.prev = newNode;
+            }
+            this.head = newNode;
+            if (this.size === 0) this.tail = newNode;
+        } else if (index === this.size) {
+            // Insert at tail
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        } else {
+            // Insert in the middle
+            const previousNode = this.getByIndex(index - 1);
+            const nextNode = previousNode.next;
+
+            previousNode.next = newNode;
+            newNode.prev = previousNode;
+            newNode.next = nextNode;
+            nextNode.prev = newNode;
+        }
+
+        this.size++;
+    }
+
+    removeByIndex(index) {
+        if (index < 0 || index >= this.size) return;
+
+        if (index === 0) {
+            // Remove head
+            this.head = this.head.next;
+            if (this.head) this.head.prev = null;
+            else this.tail = null; // list is now empty
+        } else if (index === this.size - 1) {
+            // Remove tail
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+        } else {
+            // Remove in middle
+            const nodeToRemove = this.getByIndex(index);
+            const prevNode = nodeToRemove.prev;
+            const nextNode = nodeToRemove.next;
+
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+        }
+
+        this.size--;
+    }
+}
+
+const list = new DoublyLinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.addByIndex(1, 5);
+list.removeByIndex(1);
+
+console.log(list.getByIndex(1));
+console.log(list);
+
 
